816912686126ce30795e6f03afd86e606153b4d0
Nathaniel Moschkin <theimprobable@hotmail.com>
2022-04-06 22:28:56 -0400

    Initial Commit

---
+++ CSRefectorCurio/Build/MSBuildTool.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTools.CSTools
+{
+
+
+    public class MSBuildTool
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+
+        public const string FindMSBuildCmd = "\"%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe\"";
+        public const string FindMSBuildParams = "-latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe";
+
+        private string msBuild;
+
+        public MSBuildTool()
+        {
+            msBuild = FindMSBuild();
+        }
+
+
+        public bool RunMSBuild(ProjectReader project)
+        {
+            var cmd = $"\"{project.ProjectRoot}\\{project.ProjectFile}\" -t:Rebuild /property:Configuration=Debug /property:Platform=\"AnyCPU\"";
+
+            var proc = new Process();
+
+            proc.StartInfo.WorkingDirectory = project.ProjectRoot;
+            proc.StartInfo.FileName = msBuild;
+            proc.StartInfo.Arguments = cmd;
+            proc.StartInfo.CreateNoWindow = false;
+
+            proc.Start();
+            proc.WaitForExit();
+            
+            return proc.ExitCode == 0;
+          
+        }
+
+
+
+        public string FindMSBuild()
+        {
+            var proc = new Process();
+
+            proc.StartInfo.FileName = Environment.ExpandEnvironmentVariables(FindMSBuildCmd);
+            proc.StartInfo.Arguments = FindMSBuildParams;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+
+            proc.Start();
+            proc.WaitForExit();
+
+            var strm = proc.StandardOutput.ReadToEnd();
+
+            return strm.Replace("\r", "").Replace("\n", "");
+        }
+
+    }
+}
---
+++ CSTools/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CSRefectorCurio")]
+[assembly: InternalsVisibleTo("CSRefactorOddities")] 
+[assembly: InternalsVisibleTo("LongSourceUtility")]
