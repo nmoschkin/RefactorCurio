5e8a882fac6c5c78ea9f3437d734bff0ee3b2598
Nathaniel Moschkin <theimprobable@hotmail.com>
2023-01-23 01:07:23 -0500

    Split files

--- DataTools.Code/Code/Project/IProjectElement.cs
+++ DataTools.Code/Code/Project/IProjectElement.cs
@@ -3,22 +3,6 @@
 namespace DataTools.Code.Project
 {
     /// <summary>
-    /// Represents the basis for all solution elements.
-    /// </summary>
-    public interface ISolutionElement
-    {
-        /// <summary>
-        /// The element type.
-        /// </summary>
-        ElementType ElementType { get; }
-
-        /// <summary>
-        /// The title of the element.
-        /// </summary>
-        string Title { get; }
-    }
-
-    /// <summary>
     /// Represents the basis for all project elements.
     /// </summary>
     public interface IProjectElement : ISolutionElement
---
+++ DataTools.Code/Code/Project/ISolutionElement.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTools.Code.Project
+{
+    /// <summary>
+    /// Represents the basis for all solution elements.
+    /// </summary>
+    public interface ISolutionElement
+    {
+        /// <summary>
+        /// The element type.
+        /// </summary>
+        ElementType ElementType { get; }
+
+        /// <summary>
+        /// The title of the element.
+        /// </summary>
+        string Title { get; }
+    }
+}
